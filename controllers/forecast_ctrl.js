2c52e190c219e710d4eb1fd308112973f8e9d43e
San Guyer <sanguyer@gmail.com>
2018-01-06 02:14:00 -0500

    weatherService returns an object array

--- controllers/forecast_ctrl.js
+++ controllers/forecast_ctrl.js
@@ -1,11 +1,20 @@
 (function() {
 
   angular.module('ngWeather')
-    .controller('forecastCtrl', ['$scope', 'weatherService', 
+    .controller('forecastCtrl', ['$scope', '$routeParams', 'weatherService',
       
-      function($scope, weatherService) {
+      function($scope, $routeParams, weatherService) {
+
+        $scope.days = $routeParams.days || '7';
 
         $scope.city = weatherService.city;
+
+        $scope.forecast = weatherService.forecastAPI.get({
+          q: $scope.city,
+          days: $scope.days
+        });
+
+        console.log($scope.forecast);
 
       }]);
 
--- services/weather_service.js
+++ services/weather_service.js
@@ -1,9 +1,15 @@
 (function() {
 
   angular.module('ngWeather')
-    .service('weatherService', [function() {
+    .service('weatherService', ['$resource', function($resource) {
 
       this.city = '';
+
+      this.forecastAPI = $resource(
+        "https://api.apixu.com/v1/forecast.json?key=b8962b87a64949129d445924180501"
+      );
+
+
 
     }]);
 
